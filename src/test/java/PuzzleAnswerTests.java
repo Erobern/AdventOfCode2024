07c214161ad9c42f618c8e900aa77f442e3f2113
Garrett Rieger <garrettrieger@gmail.com>
2024-12-03 10:06:07 -0600

    remove tests

--- src/test/java/PuzzleAnswerTests.java
+++ src/test/java/PuzzleAnswerTests.java
@@ -1,14 +1,8 @@
-import adventofcode2024.solvers.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
 public class PuzzleAnswerTests {
 
-    @Test
-    public void AllPuzzleSolutions() {
-        Assertions.assertEquals("", Day1.Puzzle1("Day1_1.txt"), "Day 1, Puzzle 1");
-        Assertions.assertEquals("", Day1.Puzzle2("Day1_1.txt"), "Day 1, Puzzle 2");
-    }
+//    @Test
+//    public void AllPuzzleSolutions() {
+//        Assertions.assertEquals("", Day1.Puzzle1("Day1_1.txt"), "Day 1, Puzzle 1");
+//        Assertions.assertEquals("", Day1.Puzzle2("Day1_1.txt"), "Day 1, Puzzle 2");
+//    }
 }
