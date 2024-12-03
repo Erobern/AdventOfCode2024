6d81936081fce02deb12e2034af118afcb12d634
Garrett Rieger <garrettrieger@gmail.com>
2024-12-02 23:30:27 -0600

    Day 3 + cleanup

--- src/main/java/adventofcode2024/Main.java
+++ src/main/java/adventofcode2024/Main.java
@@ -2,6 +2,7 @@ package adventofcode2024;
 
 import adventofcode2024.solvers.Day1;
 import adventofcode2024.solvers.Day2;
+import adventofcode2024.solvers.Day3;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class Main {
 
         System.out.println("Day2/Puzzle1: " + Day2.Puzzle1("Day2_1.txt"));
         System.out.println("Day2/Puzzle2: " + Day2.Puzzle2("Day2_1.txt"));
+
+        System.out.println("Day3/Puzzle1: " + Day3.Puzzle1("Day3_1.txt"));
+        System.out.println("Day3/Puzzle2: " + Day3.Puzzle2("Day3_1.txt"));
     }
 }
---
+++ src/main/java/adventofcode2024/solvers/Day3.java
@@ -0,0 +1,61 @@
+package adventofcode2024.solvers;
+
+import adventofcode2024.fileloaders.FileLoaders;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day3 {
+    public static String Puzzle1(String input) {
+        List<String> lines = FileLoaders.loadInputIntoStringList(input);
+
+        AtomicReference<Long> sum = new AtomicReference<>(0L);
+
+        Pattern pattern = Pattern.compile("mul\\([0-9]*,[0-9]*\\)");
+
+        lines.forEach(line -> {
+            Matcher matcher = pattern.matcher(line);
+            matcher.results().forEach(match -> {
+                List<String> multiplicands = Arrays.stream(match.group()
+                        .replaceAll("mul\\(", "")
+                        .replaceAll("\\)", "")
+                        .split(",")).toList();
+
+                sum.updateAndGet(v -> v + multiplicands.stream().map(Long::parseLong).reduce(1L, (a, b) -> a * b));
+            });
+        });
+        return sum.toString();
+    }
+
+    public static String Puzzle2(String input) {
+        List<String> lines = FileLoaders.loadInputIntoStringList(input);
+
+        AtomicReference<Long> sum = new AtomicReference<>(0L);
+        AtomicBoolean enabled = new AtomicBoolean(true);
+
+        Pattern pattern = Pattern.compile("(mul\\([0-9]*,[0-9]*\\))|(do\\(\\))|(don't\\(\\))");
+
+        lines.forEach(line -> {
+            Matcher matcher = pattern.matcher(line);
+            matcher.results().forEach(match -> {
+                if (match.group().contains("do()")) {
+                    enabled.set(true);
+                } else if (match.group().contains("don't()")) {
+                    enabled.set(false);
+                } else if (match.group().contains("mul") && enabled.get()) {
+                    List<String> multiplicands = Arrays.stream(match.group()
+                            .replaceAll("mul\\(", "")
+                            .replaceAll("\\)", "")
+                            .split(",")).toList();
+
+                    sum.updateAndGet(v -> v + multiplicands.stream().map(Long::parseLong).reduce(1L, (a, b) -> a * b));
+                }
+            });
+        });
+        return sum.toString();
+    }
+}
