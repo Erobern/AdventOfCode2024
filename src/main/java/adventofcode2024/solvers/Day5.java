f2cc8b0dc873203affff02d9c1854cb5597ffa3f
Garrett Rieger <garrett.rieger@fcc-fac.ca>
2024-12-05 13:58:30 -0600

    day 5 part 2

--- src/main/java/adventofcode2024/solvers/Day5.java
+++ src/main/java/adventofcode2024/solvers/Day5.java
@@ -101,11 +101,84 @@ public class Day5 {
             if (!validPrintOrder) {
                 invalidPrintOrders.add(printOrder);
             }
+        });
 
+        AtomicReference<Integer> fixedOrdersSum = new AtomicReference<>(0);
+
+        invalidPrintOrders.forEach(invalidPrintOrder -> {
+            List<List<Integer>> candidateFixedPrintOrders = new ArrayList<>();
+            List<Rule> applicableRules = new ArrayList<>();
+
+            for (Rule rule : rules) {
+                // does invalid print order contain both pages in the rule?
+                if (invalidPrintOrder
+                        .stream()
+                        .filter(page -> page.equals(rule.beforePageNumber) || page.equals(rule.afterPageNumber))
+                        .count() == 2) {
+                    applicableRules.add(rule);
+                }
+            }
+
+            // build the first candidate list with the first applicable rule
+            candidateFixedPrintOrders.add(List.of(applicableRules.getFirst().beforePageNumber,
+                    applicableRules.getFirst().afterPageNumber));
+
+            List<Integer> remainingPages = new ArrayList<>();
+            remainingPages.addAll(invalidPrintOrder.stream().filter(page ->
+                    !(page.equals(applicableRules.getFirst().beforePageNumber) ||
+                    page.equals(applicableRules.getFirst().afterPageNumber))).toList());
+
+            while (!remainingPages.isEmpty()) {
+                Integer candidatePage = remainingPages.removeFirst();
+
+                List<List<Integer>> newCandidateFixedPrintOrders = new ArrayList<>();
+
+                // set up potential candidates
+                candidateFixedPrintOrders.forEach(candidateFixedPrintOrder -> {
+                    for (int i = 0; i < candidateFixedPrintOrder.size(); i++) {
+                        List<Integer> newCandidateFixedPrintOrder = new ArrayList<>(candidateFixedPrintOrder);
+                        newCandidateFixedPrintOrder.add(i, candidatePage);
+                        newCandidateFixedPrintOrders.add(newCandidateFixedPrintOrder);
+                    }
+                    List<Integer> newCandidateFixedPrintOrder = new ArrayList<>(candidateFixedPrintOrder);
+                    newCandidateFixedPrintOrder.add(candidatePage);
+                    newCandidateFixedPrintOrders.add(newCandidateFixedPrintOrder);
+                });
+
+                candidateFixedPrintOrders.clear();
+
+                // rule them out
+                newCandidateFixedPrintOrders.forEach(newCandidateFixedPrintOrder -> {
+                    boolean validPrintOrder = true;
+
+                    for (Rule rule : rules) {
+                        // does print order contain both pages in the rule?
+                        if (newCandidateFixedPrintOrder
+                                .stream()
+                                .filter(page -> page.equals(rule.beforePageNumber) || page.equals(rule.afterPageNumber))
+                                .count() == 2) {
+                            // is print order ordered correctly?
+                            if (newCandidateFixedPrintOrder.indexOf(rule.beforePageNumber) > newCandidateFixedPrintOrder.indexOf(rule.afterPageNumber)) {
+                                validPrintOrder = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (validPrintOrder) {
+                        candidateFixedPrintOrders.add(newCandidateFixedPrintOrder);
+                    }
+                });
+
+            }
+
+            List<Integer> fixedPrintOrder = candidateFixedPrintOrders.getFirst();
+
+            fixedOrdersSum.updateAndGet(v -> v + (fixedPrintOrder.get((fixedPrintOrder.size() - 1) / 2)));
 
         });
 
-        return "";
+        return fixedOrdersSum.toString();
     }
 
     record Rule (Integer beforePageNumber, Integer afterPageNumber) {};
