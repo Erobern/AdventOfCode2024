8f61e604a99a98f970564ad4ce73ce8478d5279b
Garrett Rieger <garrettrieger@gmail.com>
2024-12-06 18:02:31 -0600

    comment out previous answers

--- src/main/java/adventofcode2024/Main.java
+++ src/main/java/adventofcode2024/Main.java
@@ -1,25 +1,23 @@
 package adventofcode2024;
 
-import adventofcode2024.solvers.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day1/Puzzle1: " + Day1.Puzzle1("Day1_1.txt"));
-        System.out.println("Day1/Puzzle2: " + Day1.Puzzle2("Day1_1.txt"));
-
-        System.out.println("Day2/Puzzle1: " + Day2.Puzzle1("Day2_1.txt"));
-        System.out.println("Day2/Puzzle2: " + Day2.Puzzle2("Day2_1.txt"));
-
-        System.out.println("Day3/Puzzle1: " + Day3.Puzzle1("Day3_1.txt"));
-        System.out.println("Day3/Puzzle2: " + Day3.Puzzle2("Day3_1.txt"));
-
-        System.out.println("Day4/Puzzle1: " + Day4.Puzzle1("Day4_1.txt"));
-        System.out.println("Day4/Puzzle2: " + Day4.Puzzle2("Day4_1.txt"));
-
-        System.out.println("Day5/Puzzle1: " + Day5.Puzzle1("Day5_1.txt"));
-        System.out.println("Day5/Puzzle2: " + Day5.Puzzle2("Day5_1.txt"));
-
-        System.out.println("Day6/Puzzle1: " + Day6.Puzzle1("Day6_1.txt"));
-        System.out.println("Day6/Puzzle2: " + Day6.Puzzle2("Day6_1.txt"));
+//        System.out.println("Day1/Puzzle1: " + Day1.Puzzle1("Day1_1.txt"));
+//        System.out.println("Day1/Puzzle2: " + Day1.Puzzle2("Day1_1.txt"));
+//
+//        System.out.println("Day2/Puzzle1: " + Day2.Puzzle1("Day2_1.txt"));
+//        System.out.println("Day2/Puzzle2: " + Day2.Puzzle2("Day2_1.txt"));
+//
+//        System.out.println("Day3/Puzzle1: " + Day3.Puzzle1("Day3_1.txt"));
+//        System.out.println("Day3/Puzzle2: " + Day3.Puzzle2("Day3_1.txt"));
+//
+//        System.out.println("Day4/Puzzle1: " + Day4.Puzzle1("Day4_1.txt"));
+//        System.out.println("Day4/Puzzle2: " + Day4.Puzzle2("Day4_1.txt"));
+//
+//        System.out.println("Day5/Puzzle1: " + Day5.Puzzle1("Day5_1.txt"));
+//        System.out.println("Day5/Puzzle2: " + Day5.Puzzle2("Day5_1.txt"));
+//
+//        System.out.println("Day6/Puzzle1: " + Day6.Puzzle1("Day6_1.txt"));
+//        System.out.println("Day6/Puzzle2: " + Day6.Puzzle2("Day6_1.txt"));
     }
 }
